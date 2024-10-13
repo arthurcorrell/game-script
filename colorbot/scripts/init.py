90af74cc8cf340f96391fd5603b1517ffabf94cd
arthurcorrell <arthurcorrell@gmail.com>
2024-10-12 19:33:08 -0600

    added multiprocessing

--- colorbot/scripts/init.py
+++ colorbot/scripts/init.py
@@ -1,1 +1,18 @@
-# initializes scripts
+import multiprocessing
+import aim
+import trigger
+
+def run_aim():
+    aim.send_coordinates()
+
+def run_trigger():
+    trigger.send_bool()
+
+aim_process = multiprocessing.Process(target=run_aim)
+trigger_process = multiprocessing.Process(target=run_trigger)
+
+aim_process.start()
+trigger_process.start()
+
+aim_process.join()
+trigger_process.join()
